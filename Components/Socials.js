887072dddc949438df251ee2f5c37cca086674df
Aditya Pawar <31154710+216aditya4565@users.noreply.github.com>
2024-05-29 16:11:31 -0400

    Update Socials.js

--- Components/Socials.js
+++ Components/Socials.js
@@ -2,35 +2,22 @@
 import Link from 'next/link';
 
 // icons
-import {
-  RiYoutubeLine,
-  RiInstagramLine,
-  RiFacebookLine,
-  RiDribbbleLine,
-  RiBehanceLine,
-  RiPinterestLine,
-} from 'react-icons/ri';
+import { RiLinkedinLine, RiGithubLine, RiMailLine } from 'react-icons/ri';
 
 const Socials = () => {
   return (
     <div className='flex items-center gap-x-5 text-lg'>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiYoutubeLine />
+      <Link href='https://www.linkedin.com/in/adityaanilpawar281902/' className='hover:text-accent transition-all duration-300' target="_blank" rel="noopener noreferrer">
+        <RiLinkedinLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiFacebookLine />
+      <Link href='https://github.com/216aditya4565' className='hover:text-accent transition-all duration-300' target="_blank" rel="noopener noreferrer">
+        <RiGithubLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiInstagramLine />
+      <Link href='mailto:adityaanilpawar281198@gmail.com' className='hover:text-accent transition-all duration-300'>
+        <RiMailLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiDribbbleLine />
-      </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiBehanceLine />
-      </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
-        <RiPinterestLine />
+      <Link href='https://drive.google.com/file/d/15qTfMF9aFKCXHHv9uzMLkh2XXIHGTcgh/view?usp=sharing' className='hover:text-accent transition-all duration-300' target="_blank" rel="noopener noreferrer">
+        <span className='text-lg font-bold'>Resume</span>
       </Link>
     </div>
   );
