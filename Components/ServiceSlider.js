3537510bb03893900ddbc3f8394b8e2b37f3ae21
Aditya Pawar <31154710+216aditya4565@users.noreply.github.com>
2024-05-29 16:11:04 -0400

    Update ServiceSlider.js

--- Components/ServiceSlider.js
+++ Components/ServiceSlider.js
@@ -1,12 +1,8 @@
-// import swiper react components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// import swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
-
-// icons
+import { FreeMode, Pagination } from 'swiper';
 import {
   RxCrop,
   RxDesktop,
@@ -16,35 +12,31 @@ import {
   RxArrowTopRight,
 } from 'react-icons/rx';
 
-// import required modules
-import { FreeMode, Pagination } from 'swiper';
-
 // service data
 export const serviceData = [
   {
-    icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    icon: <RxDesktop />,
+    title: 'Associate Software Engineer',
+    description: 'At Ugam, A Merkle USA Company.',
+    link: 'https://www.linkedin.com/in/adityaanilpawar281902/', // Add your LinkedIn profile URL
   },
   {
     icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: 'Graduate Research Assistant',
+    description: 'To Dr. Sergio Alvarez at University of Central Florida.',
+    link: 'https://www.linkedin.com/in/adityaanilpawar281902/', // Add your LinkedIn profile URL
+  },
+  {
+    icon: <RxPencil2 />,
+    title: 'Graduate Teaching Assistant',
+    description: 'To Dr. Travis Meade at University of Central Florida.',
+    link: 'https://www.linkedin.com/in/adityaanilpawar281902/', // Add your LinkedIn profile URL
   },
   {
     icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: 'Web Developer Intern',
+    description: 'At Suven Consultants and Technology Pvt.Ltd.',
+    link: 'https://www.linkedin.com/in/adityaanilpawar281902/', // Add your LinkedIn profile URL
   },
 ];
 
@@ -56,7 +48,6 @@ const ServiceSlider = () => {
           slidesPerView: 1,
           spaceBetween: 15,
         },
-
         640: {
           slidesPerView: 3,
           spaceBetween: 15,
@@ -72,21 +63,23 @@ const ServiceSlider = () => {
       {serviceData.map((item, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className='bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300'>
-              {/* icon */}
-              <div className='text-4xl text-accent mb-4'>{item.icon}</div>
-              {/* title & desc */}
-              <div className='mb-8'>
-                <div className='mb-2 text-lg'>{item.title}</div>
-                <p className='max-w-[350px] leading-normal'>
-                  {item.description}
-                </p>
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              <div className='bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300'>
+                {/* icon */}
+                <div className='text-4xl text-accent mb-4'>{item.icon}</div>
+                {/* title & desc */}
+                <div className='mb-8'>
+                  <div className='mb-2 text-lg'>{item.title}</div>
+                  <p className='max-w-[350px] leading-normal'>
+                    {item.description}
+                  </p>
+                </div>
+                {/* arrow */}
+                <div className='text-3xl'>
+                  <RxArrowTopRight className='group-hover:rotate-45 group-hover:text-accent transition-all duration-300' />
+                </div>
               </div>
-              {/* arrow */}
-              <div className='text-3xl'>
-                <RxArrowTopRight className='group-hover:rotate-45 group-hover:text-accent transition-all duration-300' />
-              </div>
-            </div>
+            </a>
           </SwiperSlide>
         );
       })}
